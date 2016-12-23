ac2ebbda456ed77e2695bfcca55c6e75440eca79
Duncan Paterson <dunkyp@gmail.com>
2016-12-23 00:50:03 +0000

    more basic changes to structure

---
+++ src/pages/editor.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+export default class Editor extends React.Component<{}, {}> {
+  render() {
+    return (
+      <h1>Editor</h1>
+    );
+  }
+}
